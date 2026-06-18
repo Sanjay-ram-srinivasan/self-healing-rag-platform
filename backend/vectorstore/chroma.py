f1e285a0c5523748466e3aaeeaf3ee1ad62ac881
Sanjay Ram <sanjaysrinivasan.ram@gmail.com>
2026-06-18 13:21:27 +0530

    Switch to OpenAI embeddings

--- backend/vectorstore/chroma.py
+++ backend/vectorstore/chroma.py
@@ -1,14 +1,14 @@
 from langchain_community.vectorstores import Chroma
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 import os
 
 DB_PATH = "chroma_db"
 
 def get_vectorstore():
-    embedding_model = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
-    google_api_key=os.getenv("GOOGLE_API_KEY")
-)
+    embedding_model = OpenAIEmbeddings(
+        model="text-embedding-3-small",
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
 
     return Chroma(
         persist_directory=DB_PATH,
